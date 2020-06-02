7b0074087fa2a2a83112a5817019c76e05b20568
jsmithxyz <jordansmith3813@gmail.com>
2020-06-02 16:37:07 -0400

    set up server.js, intitial test routes, and npm package d/ls

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(morgan("tiny"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/api", (req, res) => {
+  const data = {
+    name: "Jordan",
+    age: 30,
+  };
+  res.json(data);
+});
+
+app.get("/api/username", (req, res) => {
+  const data = {
+    name: "Kate",
+    age: 30,
+  };
+  res.json(data);
+});
+
+app.listen(PORT, console.log(`Server is starting at port ${PORT}`));
